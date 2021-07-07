0fc4fd8b7eb26fc7b61c83523b40fb904d5af1b8
Srishti Kumari <sri993951@gmail.com>
2021-07-07 12:54:05 +0530

    Added code for Populating Database

--- app/src/main/java/com/example/srishti/mvvmarchitectureexample/NoteDatabase.java
+++ app/src/main/java/com/example/srishti/mvvmarchitectureexample/NoteDatabase.java
@@ -1,9 +1,14 @@
 package com.example.srishti.mvvmarchitectureexample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -15,8 +20,29 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class,
                     "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull @NotNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+        private PopulateDbAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1", "description1", 1));
+            noteDao.insert(new Note("Title2", "description2", 2));
+            noteDao.insert(new Note("Title3", "description3", 3));
+            return null;
+        }
     }
 }
