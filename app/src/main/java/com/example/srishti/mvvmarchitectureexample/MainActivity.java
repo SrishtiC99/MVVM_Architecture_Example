6e3ae7ce12c65289e662ed42de106b45135fea36
Srishti Kumari <sri993951@gmail.com>
2021-07-07 14:44:00 +0530

    Connected a ViewModel to Activity

--- app/src/main/java/com/example/srishti/mvvmarchitectureexample/MainActivity.java
+++ app/src/main/java/com/example/srishti/mvvmarchitectureexample/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.srishti.mvvmarchitectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of((FragmentActivity) getApplicationContext()).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe((LifecycleOwner) this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable @org.jetbrains.annotations.Nullable List<Note> notes) {
+                // Update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
